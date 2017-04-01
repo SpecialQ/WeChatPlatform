0f6a3f1f26b67af38c105833af5580ded15bda14
SpecialQ <252051985@qq.com>
2017-04-01 17:12:20 +0800

    1、增加消息接收与响应功能。2、增加access_token获取

---
+++ src/main/java/com/xy/bean/message/TextMessage.java
@@ -0,0 +1,20 @@
+package com.xy.bean.message;
+
+/**
+ * 请求消息之文本消息
+ * 
+ * @author yangs
+ *
+ */
+public class TextMessage extends BaseMessage {
+    // 消息内容
+    private String Content;
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+}
--- src/main/java/com/xy/servlet/WeixinServlet.java
+++ src/main/java/com/xy/servlet/WeixinServlet.java
@@ -2,12 +2,18 @@ package com.xy.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dom4j.DocumentException;
+
+import com.xy.bean.message.TextMessage;
+import com.xy.util.MessageUtil;
 import com.xy.util.WeixinConnector;
 
 public class WeixinServlet extends HttpServlet {
@@ -32,11 +38,42 @@ public class WeixinServlet extends HttpServlet {
         }else{
         	System.out.println("验证失敗");
         }
+        out.close();
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/xml;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		String returnMessage = null;
+		Map<String, String> map = null;
+		try {
+			map = MessageUtil.xmlToMap(req);
+		} catch (DocumentException e) {
+			returnMessage = null;
+			e.printStackTrace();
+		}
+		String toUserName = map.get("ToUserName");
+		String fromUserName = map.get("FromUserName");
+		String msgType = map.get("MsgType");
+		String content = map.get("Content");
+
+		
+		if (MessageUtil.MSG_TYPE_TEXT.equals(msgType)) { // 对文本消息进行处理
+			TextMessage text = new TextMessage();
+			text.setFromUserName(toUserName); // 发送和回复是反向的
+			text.setToUserName(fromUserName);
+			text.setMsgType(MessageUtil.MSG_TYPE_TEXT);
+			text.setCreateTime(new Date().getTime());
+			text.setContent("你发送的消息是：" + content);
+			returnMessage = MessageUtil.textMessageToXML(text);
+			text.setContent("你发送的消息是：" + returnMessage);
+			returnMessage = MessageUtil.textMessageToXML(text);
+			System.out.println(returnMessage);
+		}
+		out.print(returnMessage); // 将回应发送给微信服务器
+		out.close();
 	}
 
 }
